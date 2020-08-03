3954160d443767ff69a4c22e8825911d79ba7255
akshay <akshay.dr.11@gmail.com>
2020-08-03 20:18:01 +0200

    Documented node pose function

--- get_node_pose.m
+++ get_node_pose.m
@@ -1,49 +1,33 @@
-%% Read Node Points from an image
+%% Read Node Points from an image 
+%%% Written by: Akshay Dhonthi, 2020
 
-pts = readPoints('electrode_pos.JPG', 64);
+pts = readPoints('electrode_pos.JPG', 21);
 
 function pts = readPoints(image, n)
-%readPoints   Read manually-defined points from image
-%   POINTS = READPOINTS(IMAGE) displays the image in the current figure,
-%   then records the position of each click of button 1 of the mouse in the
-%   figure, and stops when another button is clicked. The track of points
-%   is drawn as it goes along. The result is a 2 x NPOINTS matrix; each
-%   column is [X; Y] for one point.
-% 
-%   POINTS = READPOINTS(IMAGE, N) reads up to N points only.
-if nargin < 2
-    n = Inf;
-    pts = zeros(2, 0);
-else
-    pts = zeros(2, n);
-end
-imshow(image);     % display image
-xold = 0;
-yold = 0;
-k = 0;
-hold on;           % and keep it there while we plot
-while 1
-    [xi, yi, but] = ginput(1);      % get a point
-    if ~isequal(but, 1)             % stop if not button 1
-        break
+    % @params
+    %     image:            Input Image
+    %         n:            Number of points to obtain
+    %
+    % @return
+    %       pts:            2 x n array of pixel points
+
+    imshow(image);     % Show Image
+    ind = 0;             
+    hold on;           % Show image unit the loop is complete
+    
+    while 1
+        [xi, yi, ~] = ginput(1);      % Get one point
+        ind = ind + 1;
+        pts(1,ind) = xi;
+        pts(2,ind) = yi;
+        plot(xi, yi, 'go');           % Plot the point on image
+        if ind == n
+            break
+        end
     end
-    k = k + 1;
-    pts(1,k) = xi;
-    pts(2,k) = yi;
-      if xold
-          plot([xold xi], [yold yi], 'go-');  % draw as we go
-      else
-          plot(xi, yi, 'go');         % first point on its own
-      end
-      if isequal(k, n)
-          break
-      end
-      xold = xi;
-      yold = yi;
-end
-hold off;
-if k < size(pts,2)
-    pts = pts(:, 1:k);
-end
+    hold off
+    if ind < size(pts,2)
+        pts = pts(:, 1:ind);
+    end
 
 end
